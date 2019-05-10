72dfd28f70382b05d3d10811bf7c203baea6dd8a
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-09 18:16:49 -0700

    - finished functionality

--- main.cpp
+++ main.cpp
@@ -32,8 +32,92 @@
  *
  */
 #include <iostream>
+using namespace std;
+
+// abstract class
+class AbstractSeq
+{
+public:
+    // Pure Virtual Function
+    virtual int fun(int k) = 0;
+
+    /*
+     * function prints sequence from position k all the way through m.
+     * PRE-CONDITION: k < m
+     * POST-CONDITION: output is displayed to user
+     */
+    void printSeq(int k, int m) {
+        // check for good data
+        if (k >= m) {
+            cout << "ERROR: K >= M!" << endl;
+            return;
+        }
+
+        // print the sequence at the segment
+        cout << "\tprintSeq() CALLED!: ";
+        for (int i = k; i <= m; i++) {
+            cout << fun(i) << " ";
+        }
+        cout << "\n\n";
+        return;
+    }
+
+    /*
+     * function sums the sequence from position k all the way through m.
+     * PRE-CONDITION: k < m
+     * POST-CONDITION: output is displayed to the user
+     */
+    void sumSeq(int k, int m) {
+        // check for good data
+        if (k >= m) {
+            cout << "ERROR: K >= M!" << endl;
+            return;
+        }
+
+        // add up the sequence at the segment
+        int result = 0;
+        for (int i = k; i <= m; i++) {
+            result += fun(i);
+        }
+        cout << "\tsumSeq() CALLED!: " << result << endl << endl;
+        return;
+    }
+
+};
+
+// This class inherits from AbstractSeq and implements fun()
+class OddSequence : public AbstractSeq
+{
+public:
+    int fun(int k) {
+        return 2 * k + 1;
+    }
+};
+
+// This class inherits from AbstractSeq and implements fun()
+class EvenSequence : public AbstractSeq
+{
+public:
+    int fun(int k) {
+        return 2 * k + 2; // since 0 is neither even or odd
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cout << "Welcome to the demo of project 3!" << endl << endl;
+
+    OddSequence os;
+    EvenSequence es;
+
+    cout << "ODD SEQUENCE: 1, 3, 5, 7, ...." << endl;
+    os.printSeq(0,5);
+    os.sumSeq(0,5);
+
+    cout << "\nEVEN SEQUENCE: 2, 4, 6, 8, ...." << endl;
+    es.printSeq(0,5);
+    es.sumSeq(0,5);
+
+    cout << "\nThis concludes the demo for this project." << endl;
     return 0;
 }
